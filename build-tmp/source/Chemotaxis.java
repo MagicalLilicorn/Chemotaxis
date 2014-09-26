818b9b53886e2884fc00866c3009b24c9843e052
MagicalLilicorn <lily4ever@msn.com>
2014-09-26 11:43:26 -0700

    feed da poro

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,8 +1,24 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Poro [] stuffs;
 Cookie food;
 int cookieColor = color(115,57,0);
 
-void setup()   
+public void setup()   
  {  
  	size(600,600);
   stuffs = new Poro[100];
@@ -11,7 +27,7 @@ void setup()
     stuffs[i] = new Poro();
   }
  }   
- void draw()   
+ public void draw()   
  {  
   background(179,221,255);
   food.show();
@@ -71,7 +87,7 @@ class Cookie {
     myX = x;
     myY = y;
   }
-  void show(){
+  public void show(){
     noStroke();
     fill(cookieColor);
     ellipse(myX,myY,30,30);
@@ -79,7 +95,7 @@ class Cookie {
     ellipse(myX,myY,22,22);
     ellipse(myX,myY,20,20);
   }
-  void disappear(){
+  public void disappear(){
     textSize(50);
     text("<3",myX,myY);
     myX = (int)(Math.random()*600);
@@ -95,7 +111,7 @@ class Poro {
     myX = (int)(Math.random()*600);
     myY = (int)(Math.random()*600);
  	}
- 	void show(){
+ 	public void show(){
  		fill(255);
     noStroke();
     ellipse(myX,myY,20,20);
@@ -112,3 +128,12 @@ class Poro {
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
